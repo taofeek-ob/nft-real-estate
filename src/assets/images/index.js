d15a71c8333f74413a273634d43fca392bd987fb
taofeek-ob <taofeek01@yahoo.com>
2022-11-12 17:02:22 +0100

    refactor: chnage banner images to svg

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,5 @@
 export { default as Logo } from "./logo.png";
-export { default as Mbtoken } from "./Mbtoken.png";
-export { default as Metamask } from "./Metamask.png";
-export { default as Opensea } from "./Opensea.png";
+export { default as Mbtoken } from "./MbToken.svg";
+export { default as Metamask } from "./Metamask.svg";
+export { default as Opensea } from "./Opensea.svg";
 export { default as HeroImage } from "./hero.png";
