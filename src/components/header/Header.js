f9fe2bd769f78a1d49a7ae806909ee8baf7e3193
taofeek-ob <taofeek01@yahoo.com>
2022-11-15 11:47:02 +0100

    feat: mobile hamburger

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Logo } from "../../assets/images/";
 import { Link } from "react-router-dom";
 import "./header.scss";
 import Modal from "../modal/Modal";
 const Header = () => {
   const [open, setOpen] = useState(false);
+  // const [check, setCheck] = useState(false);
+  const checkbox = useRef();
   const body = document.querySelector("body");
+
+  const nav = () => {
+    // setCheck(!check);
+    checkbox.current.checked = false;
+  };
+
+  // if ((checkbox.current.checked = true)) {
+  //   body.style = `overflow-y:hidden`;
+  // }
   const showModal = () => {
     setOpen(true);
 
@@ -18,6 +29,46 @@ const Header = () => {
   return (
     <>
       <header>
+        <div>
+          <input
+            type="checkbox"
+            ref={checkbox}
+            id="main-navigation-toggle"
+            className="btn btn--close"
+            title="Toggle main navigation"
+            // checked={check}
+            // onChange={(e) => setCheck(!check)}
+          />
+          <label for="main-navigation-toggle">
+            <span></span>
+          </label>
+
+          <nav id="main-navigation" className="nav-main">
+            <ul className="menu">
+              <li className="menu__item">
+                <Link to="/" className="menu__link" onClick={nav}>
+                  Home
+                </Link>
+              </li>
+              <li className="menu__item">
+                <Link to="/place-to-stay" className="menu__link" onClick={nav}>
+                  Place to stay
+                </Link>
+              </li>
+              <li className="menu__item">
+                <a className="menu__link" href="#0" onClick={nav}>
+                  NFts
+                </a>
+              </li>
+              <li className="menu__item">
+                <a className="menu__link" href="#0" onClick={nav}>
+                  Community
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
         <img src={Logo} alt="" />
         <div className="wrapper">
           <Link to="/">Home</Link>
